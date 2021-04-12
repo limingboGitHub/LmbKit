6b38e6dc6d74c0ed9df567573d39fc34f9783145
Lenovo <675683354@qq.com>
2021-04-12 11:50:42 +0800

    1.0.2

--- lmbkit/src/main/java/com/lmb/lmbkit/utils/LogTool.kt
+++ lmbkit/src/main/java/com/lmb/lmbkit/utils/LogTool.kt
@@ -3,6 +3,12 @@ package com.lmb.lmbkit.utils
 import android.util.Log
 import com.lmb.lmbkit.BuildConfig
 
+class LogTool {
+    companion object{
+        var logEnable = true
+    }
+}
+
 fun logD(tag:String,msg:String){
     if (BuildConfig.DEBUG)
         Log.d(tag,msg)
